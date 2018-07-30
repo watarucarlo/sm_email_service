5a0cecd84753d326b8a6c71b267121f2b381f3c6
Wataru Nakayama <wcnakayama@gmail.com>
2018-07-30 22:56:30 +1000

    SM8 - Validations added

--- src/test/java/com/au/siteminder/model/EmailRequestMother.java
+++ src/test/java/com/au/siteminder/model/EmailRequestMother.java
@@ -8,12 +8,12 @@ public class EmailRequestMother {
 
     public static EmailRequest build() {
         EmailRequest emailRequest = new EmailRequest();
-        emailRequest.setFrom("test");
+        emailRequest.setFrom("test@test.com");
         emailRequest.setSubject("test");
         emailRequest.setText("test");
-        emailRequest.setTo(Collections.singletonList("test@test.com"));
-        emailRequest.setCc(Collections.singletonList("test@test.com"));
-        emailRequest.setBcc(Collections.singletonList("test@test.com"));
+        emailRequest.setTo(Collections.singletonList("test1@test.com"));
+        emailRequest.setCc(Collections.singletonList("test2@test.com"));
+        emailRequest.setBcc(Collections.singletonList("test3@test.com"));
         return emailRequest;
     }
 }
